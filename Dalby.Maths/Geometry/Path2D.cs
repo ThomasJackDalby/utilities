1dd8078f92fac68fc324f15ff95a9d79aaa4d613
Tom Dalby <thomasjackdalby@gmail.com>
2024-04-24 06:20:58 +0100

    Migrated the math classes to separate repo

---
+++ Dalby.Maths/Geometry/Path2D.cs
@@ -0,0 +1,16 @@
+﻿namespace Dalby.Maths.Geometry
+{
+    public record Path2D(params Vector2D[] Points)
+    {
+        public IEnumerable<Segment2D> ToSegments()
+        {
+            Vector2D? previous = Points.FirstOrDefault();
+            if (previous is null) yield break;
+            foreach (Vector2D point in Points.Skip(1))
+            {
+                yield return new Segment2D(point, previous);
+                previous = point;
+            }
+        }
+    }
+}
