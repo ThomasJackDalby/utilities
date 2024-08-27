c1fae49ec00e1d5034fc112881a04a3602ea52b6
Tom Dalby <thomasjackdalby@gmail.com>
2024-08-27 19:35:37 +0100

    Added Dot and Abs to the Vector2D class

--- Dalby.Maths/Geometry/Vector2D.cs
+++ Dalby.Maths/Geometry/Vector2D.cs
@@ -2,7 +2,9 @@
 {
     public record Vector2D(double X, double Y)
     {
+        public double Dot(Vector2D other) => X * other.X + Y * other.Y;
         public double Cross(Vector2D other) => X * other.Y - Y * other.X;
+        public double Abs() => Math.Sqrt(X * X + Y * Y);
 
         public static Vector2D operator +(Vector2D a, Vector2D b) => new(a.X + b.X, a.Y + b.Y);
         public static Vector2D operator -(Vector2D a, Vector2D b) => new(a.X - b.X, a.Y - b.Y);
